975e30bd755e8915357a3b55273f7dde3e1a2d08
Kirk <ksahdo10@gmail.com>
2025-07-29 16:02:45 -0400

    feat: add login view

---
+++ src/features/auth/views/LoginView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import AuthLayout from '../components/AuthLayout.vue'
+import InputComponent from '@/shared/components/input/InputComponent.vue'
+import ButtonComponent from '@/shared/components/button/ButtonComponent.vue'
+
+const login = reactive({
+  email: '',
+  password: '',
+})
+
+const handleSubmit = (event: Event) => {
+  event.preventDefault()
+  console.log('Logging in with:', login)
+}
+</script>
+
+<template>
+  <AuthLayout>
+    <template #title>Faça login em sua conta</template>
+
+    <form class="w-full flex flex-col gap-5" @submit="handleSubmit">
+      <InputComponent
+        id="email"
+        label="Endereço de email"
+        v-model="login.email"
+        type="email"
+        required
+      />
+
+      <InputComponent
+        id="password"
+        label="Senha"
+        v-model="login.password"
+        type="password"
+        required
+      />
+
+      <ButtonComponent class="mt-6" type="submit"> Entrar </ButtonComponent>
+
+      <p class="mt-4 text-center text-sm text-text">
+        Não tem uma conta?
+        <a href="/register" class="text-primary-500 hover:underline">Registrar</a>
+      </p>
+    </form>
+  </AuthLayout>
+</template>
