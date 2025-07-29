e602ae6d1973b554aaea2ca3b1a0bef7381965e5
Kirk <ksahdo10@gmail.com>
2025-07-29 16:03:47 -0400

    feat: add auth router

---
+++ src/features/auth/router/index.ts
@@ -0,0 +1,16 @@
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/RegisterView.vue'),
+  },
+]
+
+export default routes
