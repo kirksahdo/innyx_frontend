34cd39d62745702fa8a9f7eb836cc3afd1bf68cd
Kirk <ksahdo10@gmail.com>
2025-07-29 16:03:59 -0400

    feat: add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import authRoutes from '../features/auth/router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [...authRoutes],
 })
 
 export default router
