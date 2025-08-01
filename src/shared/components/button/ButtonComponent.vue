3259a3ed543716316105e380359a10798b9afa15
Kirk <ksahdo10@gmail.com>
2025-08-01 18:00:17 -0400

    feat: add danger and not full option in button component

--- src/shared/components/button/ButtonComponent.vue
+++ src/shared/components/button/ButtonComponent.vue
@@ -1,8 +1,12 @@
 <template>
   <button
     :type="props.type"
-    class="flex w-full cursor-pointer justify-center rounded-md bg-primary-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 gap-2"
-    :class="{ 'opacity-50 cursor-not-allowed': props.disabled }"
+    class="flex cursor-pointer justify-center items-center rounded-md bg-primary-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 gap-2"
+    :class="{
+      'opacity-50 cursor-not-allowed': props.disabled,
+      'w-full': !props.notFull,
+      'bg-red-500 hover:bg-red-600': props.danger,
+    }"
     :disabled="props.disabled"
     @click="() => props.onClick?.()"
   >
@@ -15,6 +19,8 @@ interface Props {
   type: HTMLButtonElement['type']
   disabled?: boolean
   onClick?: () => void
+  notFull?: boolean
+  danger?: boolean
 }
 
 const props = defineProps<Props>()
