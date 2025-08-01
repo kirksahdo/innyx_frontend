639c20763a21395fc34ace6142ef09d5456c9000
Kirk <ksahdo10@gmail.com>
2025-08-01 17:59:58 -0400

    refactor: input component to solve another input types

--- src/shared/components/input/InputComponent.vue
+++ src/shared/components/input/InputComponent.vue
@@ -5,23 +5,42 @@
     }}</label>
     <div class="mt-2">
       <input
+        v-if="props.as === 'input'"
         :id="props.id"
         v-model="model"
         :type="props.type"
         :required="props.required"
+        :step="props.step"
         class="block w-full rounded-md border-0 py-1.5 text-textshadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       />
+      <textarea
+        v-else-if="props.as === 'textarea'"
+        :id="props.id"
+        v-model="model"
+        :required="props.required"
+        rows="3"
+        class="block w-full rounded-md border-0 py-1.5 text-textshadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      ></textarea>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
 interface Props {
   label: string
   id: string
   required?: boolean
   type?: HTMLInputElement['type']
+  as?: 'input' | 'textarea'
+  step?: string | number
 }
 
-const props = defineProps<Props>()
-const model = defineModel<string>()
+const props = withDefaults(defineProps<Props>(), {
+  as: 'input',
+  type: 'text',
+  required: false,
+})
+const model = defineModel<string | number>()
 </script>
